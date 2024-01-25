83fe57bd2769e4a8fadfa8e1a62c908a804f11bc
fontainejp <fontaine.jp@gmail.com>
2024-01-25 19:47:37 +0100

    version 1.9.5

--- js/ffau/ffau.js
+++ js/ffau/ffau.js
@@ -142,6 +142,72 @@ window.addEventListener('load', function load(event) {
 			}, 'text')
 		}
 	})
+	$('#btn_media1').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('audios')
+		rootBlock.setFieldValue("acqua.mp3", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media2').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("allume.png", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media3').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('video_file')
+		rootBlock.setFieldValue("aube.mp4", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media4').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("beige.gif", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media5').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("college.jpg", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media6').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("earth.jpg", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media7').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("eteint.png", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media8').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("neuronne.jpg", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media9').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("ordinateur.png", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media10').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("portable.png", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
+	$('#btn_media11').on('click', function(){
+		var rootBlock = ffau.ffauWorkspace.newBlock('image')
+		rootBlock.setFieldValue("zen.jpg", 'source')
+		rootBlock.initSvg()
+		rootBlock.render()
+	})
 	$('#codeORblock').on("change", function() {
 		var data = document.getElementById('blockly_r').srcdoc
 		if (window.localStorage.contentHTML=="on") {
